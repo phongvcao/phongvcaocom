ad27a988b4753a279f43f9b1eb889a29dd0d3de2
Phong V. Cao <phongvcao@phongvcao.com>
2014-09-07 00:25:44 -0400

    - Added new image and content files - Made some minor CSS modification - Resume Updated
    TODO:
    - Finish software-portfolio.html
    - Upload PathX to Github (both source code and binary)

--- phongvcaocom/static/jquery/home.js
+++ phongvcaocom/static/jquery/home.js
@@ -1,85 +1,85 @@
-$(document).ready(function() {  
-  $("#resume").fancybox({
-    fitToView : false,
-    width : 855,
-    height : 542,
-    autoSize : false,
-    closeClick : false,
-    openEffect : 'elastic',
-    closeEffect	: 'elastic'
-  });
+$(document).ready(function() {
+    $("#resume").fancybox({
+        fitToView : false,
+        width : 855,
+        height : 542,
+        autoSize : false,
+        closeClick : false,
+        openEffect : 'elastic',
+        closeEffect	: 'elastic'
+    });
 
-  var browser_userAgent = navigator.userAgent;
-  
-  if (browser_userAgent.indexOf("MSIE") >= 0) {
-    var browser_version = navigator.appVersion.match(/MSIE ([\d.]+)/)[1];
-    browser_version = parseFloat(navigator.appVersion.match(/MSIE ([\d.]+)/)[1]);
-    
-    if (browser_version < 8) {
-      $("<p style='color:red; font-family: 'Arial', san-serif; font-size: 22px; text-align: center;'><span style='font-weight:bold; text-decoration:underline;'>WARNING:</span> This page DOES NOT support IE 6/7/8 <br /> Please <a href='http://windows.microsoft.com/en-us/internet-explorer/products/ie/home/' target='_blank' style='color:white'>upgrade to IE 9</a> or <a href='http://internet-browser-review.toptenreviews.com/' target='_blank' style='color:white;'>use another browser</a> to view the website</p>").insertBefore("#first-row");
+    var browser_userAgent = navigator.userAgent;
+
+    if (browser_userAgent.indexOf("MSIE") >= 0) {
+        var browser_version = navigator.appVersion.match(/MSIE ([\d.]+)/)[1];
+        browser_version = parseFloat(navigator.appVersion.match(/MSIE ([\d.]+)/)[1]);
+
+        if (browser_version < 8) {
+            $("<p style='color:red; font-family: 'Arial', san-serif; font-size: 22px; text-align: center;'><span style='font-weight:bold; text-decoration:underline;'>WARNING:</span> This page DOES NOT support IE 6/7/8 <br /> Please <a href='http://windows.microsoft.com/en-us/internet-explorer/products/ie/home/' target='_blank' style='color:white'>upgrade to IE 9</a> or <a href='http://internet-browser-review.toptenreviews.com/' target='_blank' style='color:white;'>use another browser</a> to view the website</p>").insertBefore("#first-row");
+        }
     }
-  }
-  
-  $("#about").fancybox({
-    fitToView : false,
-    width : 820,
-    height : 500,
-    autoSize : false,
-    closeClick : false,
-    openEffect : 'elastic',
-    closeEffect	: 'elastic'
-  });
-  
-  $("#design-portfolio").fancybox({
-    fitToView : false,
-    width : 1130,
-    height : 550,
-    autoSize : false,
-    closeClick : false,
-    openEffect : 'elastic',
-    closeEffect	: 'elastic'
-  });
-  
-  $("#soft-portfolio").fancybox({
-    fitToView : false,
-    width : 1050,
-    height : 565,
-    autoSize : false,
-    closeClick : false,
-    openEffect : 'elastic',
-    closeEffect	: 'elastic'
-  });
-  
-  $("#pic-2--3_screenshots").cycle({
-    speed: 2000,
-    timeout: 2000
-  });
-  
-  $("#pic-2--3").mouseenter(function() {
-    $("#pic-2--3_screenshots").cycle('pause');
-  }).mouseleave(function() {
-    $("#pic-2--3_screenshots").cycle('resume');
-  });
-  
-  $("#pic-5--2_screenshots").cycle({
-    speed: 2000,
-    timeout: 2000
-  });
-  
-  $("#pic-5--2").mouseenter(function() {
-    $("#pic-5--2_screenshots").cycle('pause');
-  }).mouseleave(function() {
-    $("#pic-5--2_screenshots").cycle('resume');
-  });
-  
-  $("#pic-4--5_screenshots").cycle({
-    speed: 2000,
-    timeout: 2000
-  });
-  
-  $("#pic-4--5").mouseenter(function() {
-    $("#pic-4--5_screenshots").cycle('pause');
-  }).mouseleave(function() {
-    $("#pic-4--5_screenshots").cycle('resume');
-  });
+
+    $("#about").fancybox({
+        fitToView : false,
+        width : 820,
+        height : 500,
+        autoSize : false,
+        closeClick : false,
+        openEffect : 'elastic',
+        closeEffect	: 'elastic'
+    });
+
+    $("#design-portfolio").fancybox({
+        fitToView : false,
+        width : 1130,
+        height : 550,
+        autoSize : false,
+        closeClick : false,
+        openEffect : 'elastic',
+        closeEffect	: 'elastic'
+    });
+
+    $("#soft-portfolio").fancybox({
+        fitToView : false,
+        width : 1050,
+        height : 565,
+        autoSize : false,
+        closeClick : false,
+        openEffect : 'elastic',
+        closeEffect	: 'elastic'
+    });
+
+    $("#pic-2--3_screenshots").cycle({
+        speed: 2000,
+        timeout: 2000
+    });
+
+    $("#pic-2--3").mouseenter(function() {
+        $("#pic-2--3_screenshots").cycle('pause');
+    }).mouseleave(function() {
+        $("#pic-2--3_screenshots").cycle('resume');
+    });
+
+    $("#pic-5--2_screenshots").cycle({
+        speed: 2000,
+        timeout: 2000
+    });
+
+    $("#pic-5--2").mouseenter(function() {
+        $("#pic-5--2_screenshots").cycle('pause');
+    }).mouseleave(function() {
+        $("#pic-5--2_screenshots").cycle('resume');
+    });
+
+    $("#pic-4--5_screenshots").cycle({
+        speed: 2000,
+        timeout: 2000
+    });
+
+    $("#pic-4--5").mouseenter(function() {
+        $("#pic-4--5_screenshots").cycle('pause');
+    }).mouseleave(function() {
+        $("#pic-4--5_screenshots").cycle('resume');
+    });
 });
--- phongvcaocom/static/jquery/software_portfolio.js
+++ phongvcaocom/static/jquery/software_portfolio.js
@@ -1,170 +1,194 @@
-$(window).load(function() {  
-  var soft_portfolio_flow = new ImageFlow();
-  
-  soft_portfolio_flow.init({
-    ImageFlowID: "soft-portfolio-flow",
-    reflections: false,
-    reflectionP: 0.0,
-    captions: true,
-    onClick: function() {},
-    imageFocusMax: 1,
-    imageFocusM: 1.45,
-    xStep: 80,
-    opacity: true,
-    opacityArray: [10, 9, 8, 7, 6],
-    animationSpeed: 50,
-    imagesHeight: 0.5,
-    scrollbarP: 0.64
-  });
-  
-  var download_htmlStr = "Download this software";
-  var original_htmlStr = "";
-  
-  $("#software-download").click(function() {
-    switch (soft_portfolio_flow.imageID) {
-      case 0: {
-        window.open("http://phongvcao.com/software/", "_blank");
-        
-        break;
-      }
-      
-      case 1: {
-        window.open("http://dingo-project.org/", "_blank");
-        
-        break;
-      }
-      
-      case 2: {
-        window.open("http://dingo-project.org/download/", "_blank");
-        
-        break;
-      }
-      
-      case 3: {
-        window.open("http://phongvcao.com/", "_blank");
-        
-        break;
-      }
-      
-      default: {
-        window.open("http://phongvcao.com/", "_blank");
-        
-        break;
-      }
-    }  
-  }).mouseenter(function() {
-    switch (soft_portfolio_flow.imageID) {
-      case 0: {
-        download_htmlStr = "Download Portfolio";
-        
-        break;
-      }
-      
-      case 1: {
-        download_htmlStr = "Go to \"Dingo Project's Website\""
-        
-        break;
-      }
-      
-      case 2: {
-        download_htmlStr = "Download \"Dingo Media Manager\"";
-        
-        break;
-      }
-      
-      case 3: {
-        download_htmlStr = "Go to \"Phong Cao's Personal Website\"";
-        
-        break;
-      }
-      
-      default: {
-        download_htmlStr = "Download this software";
-        
-        break;
-      }
-    }
-    
-    original_htmlStr = soft_portfolio_flow.captionDiv.innerHTML;
-    
-    soft_portfolio_flow.captionDiv.innerHTML = "<div class=\"caption\"> <div class=\"title\"> " + download_htmlStr + " </div> </div>";
-  }).mouseleave(function() {
-    soft_portfolio_flow.captionDiv.innerHTML = original_htmlStr;
-  });
-  
-  
-  var view_htmlStr = "View Source Code";
-  
-  $("#view-source-code").click(function() {
-    switch (soft_portfolio_flow.imageID) {
-      case 0: {
-        window.open("https://github.com/phongvcao", "_blank");
-        
-        break;
-      }
-      
-      case 1: {
-        window.open("https://github.com/phongvcao/dingoprojectorg", "_blank");
-        
-        break;
-      }
-      
-      case 2: {
-        window.open("https://github.com/phongvcao/dingo", "_blank");
-        
-        break;
-      }
-      
-      case 3: {
-        window.open("https://github.com/phongvcao/phongvcaocom", "_blank");
-        
-        break;
-      }
-      
-      default: {
-        window.open("https://github.com/phongvcao", "_blank");
-        
-        break;
-      }
-    }  
-  }).mouseenter(function() {
-    switch (soft_portfolio_flow.imageID) {
-      case 0: {
-        view_htmlStr = "View Source Code";
-        
-        break;
-      }
-      
-      case 1: {
-        view_htmlStr = "View \"Dingo Project's Website\" Source Code"
-        
-        break;
-      }
-      
-      case 2: {
-        view_htmlStr = "View \"Dingo Media Manager\" Source Code";
-        
-        break;
-      }
-      
-      case 3: {
-        view_htmlStr = "View \"Phong Cao's Personal Website\" Source Code";
-        
-        break;
-      }
-      
-      default: {
-        view_htmlStr = "View Source Code";
-        
-        break;
-      }
-    }
-    
-    original_htmlStr = soft_portfolio_flow.captionDiv.innerHTML;
-    
-    soft_portfolio_flow.captionDiv.innerHTML = "<div class=\"caption\"> <div class=\"title\"> " + view_htmlStr + " </div> </div>";
-  }).mouseleave(function() {
-    soft_portfolio_flow.captionDiv.innerHTML = original_htmlStr;
-  });
+$(window).load(function() {
+    var soft_portfolio_flow = new ImageFlow();
+
+    soft_portfolio_flow.init({
+        ImageFlowID: "soft-portfolio-flow",
+        reflections: false,
+        reflectionP: 0.0,
+        captions: true,
+        onClick: function() {},
+        imageFocusMax: 1,
+        imageFocusM: 1.45,
+        xStep: 80,
+        opacity: true,
+        opacityArray: [10, 9, 8, 7, 6],
+        animationSpeed: 50,
+        imagesHeight: 0.5,
+        scrollbarP: 0.64
+    });
+
+    var download_htmlStr = "Download this software";
+    var original_htmlStr = "";
+
+    $("#software-download").click(function() {
+        switch (soft_portfolio_flow.imageID) {
+            case 0: {
+                window.open("http://phongvcao.com/software", "_blank");
+
+                break;
+            }
+
+            case 1: {
+                window.open("http://phongvcao.com/github", "_blank");
+
+                break;
+            }
+
+            case 2: {
+                window.open("http://dingo-project.org/download", "_blank");
+
+                break;
+            }
+
+            case 3: {
+                window.open("http://research.stonybrook.edu/", "_blank");
+
+                break;
+            }
+
+            case 4: {
+                window.open("http://ding-project.org/", "_blank");
+
+                break;
+            }
+
+            case 5: {
+                window.open("http://phongvcao.com/", "_blank");
+
+                break;
+            }
+
+            default: {
+                window.open("http://phongvcao.com/", "_blank");
+
+                break;
+            }
+        }
+    }).mouseenter(function() {
+        switch (soft_portfolio_flow.imageID) {
+            case 0: {
+                download_htmlStr = "Download Portfolio";
+
+                break;
+            }
+
+            case 1: {
+                download_htmlStr = "Download \"PathX Mini-Game\"";
+
+                break;
+            }
+
+            case 2: {
+                download_htmlStr = "Download \"Dingo Media Manager\"";
+
+                break;
+            }
+
+            case 3: {
+                download_htmlStr = "Go to \"Stony Brook Research's Website\"";
+
+                break;
+            }
+
+            case 4: {
+                download_htmlStr = "Go to \"Dingo Project's Website\"";
+
+                break;
+            }
+
+            case 5: {
+                download_htmlStr = "Go to \"Phong Cao's Personal Website\"";
+
+                break;
+            }
+
+            default: {
+                download_htmlStr = "Download this software";
+
+                break;
+            }
+        }
+
+        original_htmlStr = soft_portfolio_flow.captionDiv.innerHTML;
+
+        soft_portfolio_flow.captionDiv.innerHTML = "<div class=\"caption\"> <div class=\"title\"> " + download_htmlStr + " </div> </div>";
+    }).mouseleave(function() {
+        soft_portfolio_flow.captionDiv.innerHTML = original_htmlStr;
+    });
+
+
+    var view_htmlStr = "View Source Code";
+
+    $("#view-source-code").click(function() {
+        switch (soft_portfolio_flow.imageID) {
+            case 0: {
+                window.open("https://github.com/phongvcao", "_blank");
+
+                break;
+            }
+
+            case 1: {
+                window.open("https://github.com/phongvcao/dingoprojectorg", "_blank");
+
+                break;
+            }
+
+            case 2: {
+                window.open("https://github.com/phongvcao/dingo", "_blank");
+
+                break;
+            }
+
+            case 3: {
+                window.open("https://github.com/phongvcao/phongvcaocom", "_blank");
+
+                break;
+            }
+
+            default: {
+                window.open("https://github.com/phongvcao", "_blank");
+
+                break;
+            }
+        }
+    }).mouseenter(function() {
+        switch (soft_portfolio_flow.imageID) {
+            case 0: {
+                view_htmlStr = "View Source Code";
+
+                break;
+            }
+
+            case 1: {
+                view_htmlStr = "View \"Dingo Project's Website\" Source Code"
+
+                    break;
+            }
+
+            case 2: {
+                view_htmlStr = "View \"Dingo Media Manager\" Source Code";
+
+                break;
+            }
+
+            case 3: {
+                view_htmlStr = "View \"Phong Cao's Personal Website\" Source Code";
+
+                break;
+            }
+
+            default: {
+                view_htmlStr = "View Source Code";
+
+                break;
+            }
+        }
+
+        original_htmlStr = soft_portfolio_flow.captionDiv.innerHTML;
+
+        soft_portfolio_flow.captionDiv.innerHTML = "<div class=\"caption\"> <div class=\"title\"> " + view_htmlStr + " </div> </div>";
+    }).mouseleave(function() {
+        soft_portfolio_flow.captionDiv.innerHTML = original_htmlStr;
+    });
 });
 
